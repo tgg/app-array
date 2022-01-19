1aa925a6c0924cb10dd1df6138fcc853de5b0bf1
bloto-31 <97393190+bloto-31@users.noreply.github.com>
2022-01-19 11:59:01 +0100

    Implemting Load button to load a json file + a second json file to test

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
@@ -7,6 +7,7 @@ import createEngine, {
 	DiagramEngine,
 	PathFindingLinkFactory
 } from '@projectstorm/react-diagrams';
+import styled from '@emotion/styled';
 
 import { DemoButton, DemoWorkspaceWidget } from './DemoWorkspaceWidget';
 import { CanvasWidget } from '@projectstorm/react-canvas-core';
@@ -14,12 +15,53 @@ import { DemoCanvasWidget } from './DemoCanvasWidget';
 import { FO } from './ModelTest';
 import { SystemDiagramModel } from './SystemDiagramModel';
 import { JavaScriptExecutor } from './Executor';
+import { AppArray } from './Model';
 
-class SystemWidget extends React.Component<{ model: SystemDiagramModel; engine: DiagramEngine }, any> {
+const Input = styled.input`
+    display: none;
+`
+
+interface LoadButtonProps {
+	onModelChange: (model: SystemDiagramModel) => void;
+}
+
+const LoadButton = ({ onModelChange }: LoadButtonProps) => {
+	const inputFile = useRef(null)
+
+	const handleFileUpload = (e: any) => {
+		const { files } = e.target;
+		if (files && files.length) {
+			const file = files[0];
+			const fileReader = new FileReader();
+			fileReader.readAsText(file, "UTF-8");
+			fileReader.onload = event => {
+				const newFO = JSON.parse(event?.target?.result as string) as AppArray.Model.Application;
+				let model = new SystemDiagramModel(newFO);
+				onModelChange(model)
+
+			};
+		}
+	};
+
+	return <>
+		<Input
+			accept=".json"
+			ref={inputFile}
+			onChange={handleFileUpload}
+			type="file"
+		/>
+		<DemoButton onClick={() => (inputFile?.current as unknown as any).click()}>Load ...</DemoButton>
+	</>
+}
+
+class SystemWidget extends React.Component<{ engine: DiagramEngine }, { model: SystemDiagramModel; }> {
 	engine: DagreEngine;
 
 	constructor(props: any) {
 		super(props);
+		console.info(JSON.stringify(FO));
+		const model = new SystemDiagramModel(FO);
+		props.engine.setModel(model);
 		this.engine = new DagreEngine({
 			graph: {
 				rankdir: 'LR',
@@ -29,10 +71,14 @@ class SystemWidget extends React.Component<{ model: SystemDiagramModel; engine: 
 			},
 			includeLinks: false
 		});
+
+		this.state = {
+			model
+		}
 	}
 
 	autoDistribute = () => {
-		this.engine.redistribute(this.props.model);
+		this.engine.redistribute(this.state.model);
 		this.reroute();
 		this.props.engine.repaintCanvas();
 	};
@@ -52,11 +98,11 @@ class SystemWidget extends React.Component<{ model: SystemDiagramModel; engine: 
 
 	startAll = () => {
 		console.info(this.props);
-		let app = this.props.model.getApplication();
+		let app = this.state.model.getApplication();
 		let eventBusStartCommand = app.components[1].commands?.start!;
 		let eventBusStart = new JavaScriptExecutor().runner(eventBusStartCommand.steps);
 		let d = new TextDecoder();
-		let instance = eventBusStart({ id: 'thisEnvironment'});
+		let instance = eventBusStart({ id: 'thisEnvironment' });
 		instance.channels.out.onReceive = (data: Uint8Array | null) => {
 			if (data) {
 				console.info(d.decode(data));
@@ -68,11 +114,11 @@ class SystemWidget extends React.Component<{ model: SystemDiagramModel; engine: 
 	}
 
 	stopAll = () => {
-		let app = this.props.model.getApplication();
+		let app = this.state.model.getApplication();
 		let eventBusStopCommand = app.components[1].commands?.stop!;
 		let eventBusStop = new JavaScriptExecutor().runner(eventBusStopCommand.steps);
 		let d = new TextDecoder();
-		let instance = eventBusStop({ id: 'thisEnvironment'});
+		let instance = eventBusStop({ id: 'thisEnvironment' });
 		instance.channels.out.onReceive = (data: Uint8Array | null) => {
 			if (data) {
 				console.info(d.decode(data));
@@ -85,7 +131,12 @@ class SystemWidget extends React.Component<{ model: SystemDiagramModel; engine: 
 
 	render() {
 		return (
-			<DemoWorkspaceWidget buttons={<><DemoButton onClick={this.autoDistribute}>Re-distribute</DemoButton><DemoButton onClick={this.autoDistribute}>Load ...</DemoButton><DemoButton onClick={this.startAll}>Start All</DemoButton><DemoButton onClick={this.stopAll}>Stop All</DemoButton></>}>
+			<DemoWorkspaceWidget buttons={<><DemoButton onClick={this.autoDistribute}>Re-distribute</DemoButton><LoadButton onModelChange={(model) => {
+				this.props.engine.setModel(model);
+				this.setState({ model }, () => {
+					this.autoDistribute();
+				})
+			}} /><DemoButton onClick={this.startAll}>Start All</DemoButton><DemoButton onClick={this.stopAll}>Stop All</DemoButton></>}>
 				<DemoCanvasWidget>
 					<CanvasWidget engine={this.props.engine} />
 				</DemoCanvasWidget>
@@ -96,11 +147,8 @@ class SystemWidget extends React.Component<{ model: SystemDiagramModel; engine: 
 
 function App() {
 	let engine = createEngine();
-	console.info(JSON.stringify(FO));
-	let model = new SystemDiagramModel(FO);
-	engine.setModel(model);
 
-	return <SystemWidget model={model} engine={engine} />;
+	return <SystemWidget engine={engine} />;
 }
 
 export default App;
