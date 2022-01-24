68c11a79dae6b379641208b4d7640345d450d414
Thomas Girard <thomas.g.girard@free.fr>
2022-01-24 06:10:16 +0100

    Simplify ModelTest

--- src/ModelTest.ts
+++ src/ModelTest.ts
@@ -1,75 +1,4 @@
 import { AppArray } from './Model'
-import { Channel, Cmd, Status } from './Executor'
-import { Environment } from './Environment'
-
-function StartComponent(context: Environment): Cmd<Uint8Array,any> {
-    let startTrail = {
-        status: Status.STARTED,
-        at: new Date(),
-        by: 'me',
-        on : 'self'
-    };
-
-    return {
-        launchTrail: startTrail,
-        channels: {
-            out: new Channel<Uint8Array>('out')
-        },
-        run(args: string[]) {
-            console.log('starting %s at: %s', args.join(', '), startTrail.at.toISOString());
-
-            return new Promise((resolve, reject) => {
-                if (this.channels.out.onReceive) {
-                    let e = new TextEncoder();
-                    this.channels.out.onReceive(e.encode('Hello from'));
-                    this.channels.out.onReceive(e.encode('My callback'));
-                }
-
-                resolve({
-                    completedAt: new Date(),
-                    result: 'Bravo!'
-                })
-            })
-        }
-    }
-}
-
-function StopComponent(context: Environment): Cmd<Uint8Array,any> {
-    let startTrail = {
-        status: Status.STARTED,
-        at: new Date(),
-        by: 'me',
-        on : 'self'
-    };
-
-    return {
-        launchTrail: startTrail,
-        channels: {
-            out: new Channel<Uint8Array>('dummy')
-        },
-        run(args: string[]) {
-            console.log('stopping %s at: %s', args.join(', '), startTrail.at.toISOString());
-
-            return new Promise((resolve, reject) => {
-                if (this.channels.out.onReceive) {
-                    let e = new TextEncoder();
-                    this.channels.out.onReceive(e.encode('Bye from'));
-                    this.channels.out.onReceive(e.encode('My other callback'));
-                    this.channels.out.onReceive(e.encode('Failing now'));
-                }
-
-                reject({
-                    completedAt: new Date(),
-                    result: 'Failed!'
-                })
-            })
-        }
-    }
-}
-
-// Gruik alert!
-(window as any)['StartComponent'] = StartComponent;
-(window as any)['StopComponent'] = StopComponent;
 
 let Producer: AppArray.Model.Component = {
     id: 'Producer',
@@ -82,6 +11,10 @@ let Producer: AppArray.Model.Component = {
         stop: {
             type: 'shell',
             steps: ['/app/bin/server.sh stop']
+        },
+        status: {
+            type: 'shell',
+            steps: ['/app/bin/server.sh status']
         }
     },
     provides: [ {
@@ -97,7 +30,7 @@ let Consumer: AppArray.Model.Component = {
     commands: {
         start: {
             type: 'shell',
-            steps: ['ls -l /usr']
+            steps: ['/app/bin/batch.sh']
         }
     },
     provides: [ {
